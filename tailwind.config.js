0ff9539551be910930981b5449dec1a163594708
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-11-28 14:42:58 +0500

    feat: header and desk section is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '540': '540px'
+      }
+    },
     fontFamily: {
       'poppins': ['Poppins', 'sans-serif'],
       'rubik': ['Rubik', 'sans-serif'],
     },
+    colors: {
+      'white': '#FFFFFF', 
+      'custom_orange': '#FA5959',
+      'dark_blue': '#242A45',
+      'custom_blue': '#5267DF',
+      'custom_gray': '#9194A2',
+      'light_gray': '#F7F7F7',
+    },
+    fontSize: {
+      default_size: `13px;`,
+    },
+    lineHeight: {
+      '17': '17px',
+    },
+    letterSpacing: {
+      '1.5': '1.5px'
+    },
+    borderRadius: {
+      '5': '5px',
+    }
   },
   plugins: [],
 }
