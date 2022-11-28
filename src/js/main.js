a7da8c32443f275a32c75d4303f584d9584cddf1
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-11-28 20:39:11 +0500

    feat: section faq is prossessing

--- src/js/main.js
+++ src/js/main.js
@@ -25,3 +25,18 @@ const options = {
         console.log('tab is shown');
     }
 };
+
+let acc = document.getElementsByClassName("accordion");
+
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
